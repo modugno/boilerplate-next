77ff2307689b4d00a3fcfb49bfe980cd5e7e43aa
Guilherme Modugno <modugnoguilherme@gmail.com>
2020-12-14 08:25:38 -0300

    chore: add husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+// const foo = 'teste'
+
 export default function Home() {
   return (
     <div className={styles.container}>
